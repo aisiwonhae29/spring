d5fa88328c18860f47804da635a40e886bc2062d
aisiwonhae29 <aisiwonhae29@gmail.com>
2023-04-16 17:02:17 +0900

    login page, sketch

--- project/front-end/access_page.js
+++ project/front-end/access_page.js
@@ -1,5 +1,13 @@
 $(".logo").click(function () {
-    $(".logo_box").css("top", "0px");
-    $(".bg").css();
-  });
-  
+  $(".logo_box").css("left", "3%");
+  $(".logo_box").css("top", "3%");
+  $(".logo_box").css("bottom", "auto");
+  $(".logo_box").css("right", "auto");
+  $(".logo_box").css("background-position-x", "200px");
+  $(".logo>img").css("width", "60px");
+  $(".login_box").css("display", "flex");
+});
+
+$(".login_box>button").click(function () {
+  $(".login_box>button").css("background-color", "red");
+});
