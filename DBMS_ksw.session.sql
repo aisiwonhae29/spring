fc42fdb2b8f58d1a9fb916494544e195f0df4ccc
aisiwonhae29 <aisiwonhae29@gmail.com>
2023-05-17 11:11:22 +0900

    jquery & javascript practice

--- DBMS_ksw.session.sql
+++ DBMS_ksw.session.sql
@@ -1,13 +1,16 @@
+drop database if exists pmp;
+create database pmp;
+use pmp;
 
-CREATE TABLE todayeat (
-  id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `todayeat` (
+  id INT(11) NOT NULL AUTO_INCREMENT,
   userid CHAR(50) NOT NULL,
   shopname CHAR(200) NOT NULL,
   menuname CHAR(100) NOT NULL,
   `location` CHAR(100) NOT NULL,
-  `date DATE` NOT NULL,
-  PRIMARY KEY (id)
-);
+  `date` DATE NOT NULL,
+   PRIMARY KEY (id)
+  );
 INSERT INTO todayeat (userid, shopname, menuname, location, date)
 VALUES 
 ( 'test1', '김가네', '제육볶음', '월평동', CURDATE() ),
@@ -23,13 +26,14 @@ CREATE TABLE `member` (
   userlocation CHAR(100) NOT NULL,
   userage INT(20) NOT NULL,
   usergender CHAR(10) NOT NULL,
+  useremail char(100) NOT null,
   PRIMARY KEY (id)
 );
-INSERT INTO member ( userid, userpw, username, userlocation, userage, usergender )
+INSERT INTO member ( userid, userpw, username, userlocation, userage, usergender, useremail )
 VALUES 
-('test1', 'test1', '홍길동', 'test1', 25 , 'man'),
-('test2', 'test2', '임꺽정', 'test2', 35 , 'man'),
-('test3', 'test3', '심사임', 'test3', 45 , 'woman');
+('test1', 'test1', '홍길동', 'test1', 25 , 'man', 'hongkil04@gmail.com'),
+('test2', 'test2', '임꺽정', 'test2', 35 , 'man', 'lgj992@hanmail.net'),
+('test3', 'test3', '심사임', 'test3', 45 , 'woman', 'llmom44@lycos.com');
 
 
 CREATE TABLE `location` (
@@ -55,11 +59,10 @@ CREATE TABLE shop (
   shopname CHAR(100) NOT NULL,
   locationid INT(11) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (locationid) REFERENCES your_location_table_name(locationid)
+  FOREIGN KEY (locationid) REFERENCES location(id)
 );
 INSERT INTO shop ( shopname, locationid)
 VALUES ( '김가네', 1 ), ('5.5닭갈비',1), ('동원칼국수',1);
-
 
 SELECT * from member;
 SELECT * from shop;
